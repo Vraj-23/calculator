f23ab7f104f34282b51e6f63343b10129438ada2
Vraj-23 <vraj.2402683101@vcet.edu.in>
2026-02-18 11:11:17 +0530

    Create script.js file with calculator logic

---
+++ script.js
@@ -0,0 +1,47 @@
+let display = document.getElementById('display');
+
+function appendNumber(num) {
+    if (display.value === '0' && num !== '.') {
+        display.value = num;
+    } else if (num === '.' && display.value.includes('.')) {
+        return;
+    } else {
+        display.value += num;
+    }
+}
+
+function appendOperator(op) {
+    const lastChar = display.value[display.value.length - 1];
+    if (['+', '-', '*', '/', '%'].includes(lastChar)) {
+        return;
+    }
+    display.value += op;
+}
+
+function clearDisplay() {
+    display.value = '0';
+}
+
+function deleteLast() {
+    if (display.value.length > 1) {
+        display.value = display.value.slice(0, -1);
+    } else {
+        display.value = '0';
+    }
+}
+
+function calculate() {
+    try {
+        const result = eval(display.value);
+        display.value = result;
+    } catch (error) {
+        display.value = 'Error';
+        setTimeout(() => {
+            display.value = '0';
+        }, 1500);
+    }
+}
+
+window.addEventListener('load', () => {
+    display.value = '0';
+});
